60af0f492a30394f8f47a49c03df00af68dcadcf
Tomas Baca <klaxalk@gmail.com>
2023-07-30 16:20:36 +0200

    updated github ci

--- .ci/build_package.sh
+++ .ci/build_package.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+echo "$0: building the package"
+
+ARTIFACTS_FOLDER=$1
+
+sudo apt-get install dpkg-dev
+
+echo "$0: building the package into '$ARTIFACTS_FOLDER'"
+
+mkdir -p $ARTIFACTS_FOLDER
+
 dpkg-deb --build --root-owner-group package
 dpkg-name package.deb
+
+mv *.deb $ARTIFACTS_FOLDER
